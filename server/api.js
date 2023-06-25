70bd5ff1ab5fffc68efdc2cb3ef514efe8cc98d8
Kavya R <114384085+Kavya099@users.noreply.github.com>
2023-06-25 12:47:30 +0530

    Update api.js

--- server/api.js
+++ server/api.js
@@ -27,13 +27,10 @@ mongo.connect(url,(err,db)=>{
 const axios = require('axios');
 
 
-// Register your company with John Doe Railway Server
+
 app.post('/train/register', async (req, res) => {
   try {
-    // Implement registration logic here
-    // Send the necessary registration details to John Doe Railway Server
-    // Store the access code securely for future use
-    const accessCode = 'nSFqRI'; // Replace with your actual access code
+    const accessCode = 'nSFqRI'; 
     res.json({ success: true, accessCode });
   } catch (error) {
     console.error('Error registering with John Doe Railway Server:', error);
@@ -41,40 +38,40 @@ app.post('/train/register', async (req, res) => {
   }
 });
 
-// Middleware for authentication
+
 const authenticate = (req, res, next) => {
   const accessCode = req.headers.authorization;
-  // Verify the access token matches the stored access code
-  if (accessCode !== 'nSFqRI') { // Replace with your stored access code
+
+  if (accessCode !== 'nSFqRI') { 
     return res.status(401).json({ success: false, message: 'Unauthorized' });
   }
   next();
 };
 
-// GET endpoint for retrieving train schedules with seat availability and pricing
+
 app.get('/trains', authenticate, async (req, res) => {
   try {
     const now = new Date();
     const twelveHoursFromNow = new Date(now.getTime() + 12 * 60 * 60 * 1000);
 
-    // Fetch train schedules from John Doe Railways API
+  
     const schedulesResponse = await axios.get('http://104.211.219.98/train/trains/schedules');
     const schedules = schedulesResponse.data;
 
-    // Filter trains departing in the next 30 minutes
+    
     const filteredSchedules = schedules.filter(train => {
       const departureTime = new Date(train.departureTime);
       return departureTime > now && departureTime < twelveHoursFromNow;
     });
 
-    // Fetch seat availability and pricing for each train
+    
     const trainsWithAvailabilityAndPricing = await Promise.all(filteredSchedules.map(async train => {
       const availabilityResponse = await axios.get(`http://104.211.219.98:80/interview/trains/2344/availability/${train.trainId}`);
       const pricingResponse = await axios.get(`http://104.211.219.98:80/interview/trains/2344/pricing/${train.trainId}`);
       const availability = availabilityResponse.data;
       const pricing = pricingResponse.data;
 
-      // Enhance the train data with availability and pricing
+     
       return {
         ...train,
         availability,
@@ -82,7 +79,7 @@ app.get('/trains', authenticate, async (req, res) => {
       };
     }));
 
-    // Calculate real-time values and sort the trains based on price, seat availability, and departure time
+ 
     const sortedTrains = trainsWithAvailabilityAndPricing.sort((a, b) => {
       const aPrice = a.pricing.price;
       const bPrice = b.pricing.price;
